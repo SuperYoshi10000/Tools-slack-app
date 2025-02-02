f1e7b1a9ac54a302fb76100f9d4ffabe33b62cf2
SuperYoshi10000 <superyoshi10000@gmail.com>
2025-02-02 08:59:29 -0500

    remove merge conflict tokens

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 declare interface Set<T> {
     difference<U>(other: Set<U>): Set<T>;
     intersection<U>(other: Set<U>): Set<T&U>;
@@ -8,15 +7,4 @@ declare interface Set<T> {
     isDisjointFrom(other: Set<any>): boolean;	
     isSubsetOf(other: Set<any>): boolean;	
     isSupersetOf(other: Set<any>): boolean;
-=======
-declare interface Set<T> {
-    difference<U>(other: Set<U>): Set<T>;
-    intersection<U>(other: Set<U>): Set<T&U>;
-    symmetricDifference<U>(other: Set<U>): Set<T|U>;
-    union<U>(other: Set<U>): Set<T|U>;
-
-    isDisjointFrom(other: Set<any>): boolean;	
-    isSubsetOf(other: Set<any>): boolean;	
-    isSupersetOf(other: Set<any>): boolean;
->>>>>>> 7485003502804a8b4b3d7a0e9075b52980d970d8
 }
